2d44f8948c31ee3cb41d2cafb8c83970894c2191
Lazăr Mihai <79991306+Mihai-Lazar26@users.noreply.github.com>
2023-06-29 13:53:09 +0300

    dadsad

--- The Knight/Assets/Scripts/MenuScripts/DontDestroyObject.cs
+++ The Knight/Assets/Scripts/MenuScripts/DontDestroyObject.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class DontDestroyObject : MonoBehaviour
 {
-    private void Awake() {
-        DontDestroyOnLoad(gameObject);
+    private static DontDestroyObject instance;
+
+    private void Awake()
+    {
+        // Check if an instance already exists
+        if (instance == null)
+        {
+            // If no instance exists, set this object as the instance and mark it as don't destroy on load
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // If an instance already exists, destroy this object
+            Destroy(gameObject);
+        }
     }
 }
